b59a18eacbfc2dc738bd6f6c1efd5d98f0ae5d06
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-20 13:47:41 +0000

    Final addition.

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 101-add_attribute
+Has a function that adds new attribute if it is possible. 
+"""
+
+
+def add_attribute(obj, attribute, value):
+    """
+    add attribute to object if possible
+    """
+    if ('__dict__' in dir(obj)):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
