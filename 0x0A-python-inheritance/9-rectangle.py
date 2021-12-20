68377bb16d9f57cfd08eada7bd5e6b8d76f36cce
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-20 13:12:36 +0000

    Rectangle dimensions.

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module 9-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+Contains subclass Rectangle
+with instantiation of private attributes length and breadth, validated by parent,
+extends parent's area method and prints with __str__
+"""
