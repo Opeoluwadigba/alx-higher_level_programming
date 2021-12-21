9fc9285bc05478d4903feecd78d660b1201b9c70
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-21 17:17:08 +0000

    Correction.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Module for write file function."""
-
+"""Module for write_file function."""
 
 
 def write_file(filename="", text=""):
     """This function writes a string to a text file."""
-    with open(filename, "w", encoding="utf-8.") as f:
-        written = f.write.text(text)
-        """Write return the number of characters inserted in the file."""
-        return
+    with open(filename, "w", encoding="utf-8") as f:
+        written = f.write(text)
+        """Write return the total number of characters inserts in the file."""
+        return written
