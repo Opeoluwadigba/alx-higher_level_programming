669e571cb5203c0ab1c8559fd3f82ead67670318
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-21 13:10:59 +0000

    JSON to string.

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string.py
+"""
+import json
+
+
+
+def from_json_string(my_str):
+    """Return an object represented by JSON string."""
+    return (json.loads(my_str))
