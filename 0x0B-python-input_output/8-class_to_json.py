0b34ebbcd59ab0e2237d4704e42d81bb3d5494e3
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-21 13:57:18 +0000

    Python class to JSON.

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 8-class_to_json.py"""
+
+
+
+def class_to_json(obj):
+"""
+This Function returns to the dictionary descrption
+with simple a data structure
+"""
+
+return vars(obj)
+ 
