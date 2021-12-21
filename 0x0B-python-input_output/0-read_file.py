cf60b719e1df9d9dbfb068930be08d2495674c00
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-21 17:14:09 +0000

    first chnge.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,7 @@ Module 0-read_file.py
 """
 
 
-
 def read_file(filename=""):
-    """This function reads a text file(UTF-8) and print to stdout"""
+    """This function reads a text file(UTF-8) and prints to stdout"""
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
