a5797de24e551c90481835de7ab641d7d18f255a
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-21 12:32:46 +0000

    Read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 0-read_file.py
+"""
+
+
+
+def read_file(filename=""):
+    """This function reads a text file(UTF-8) and print to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
