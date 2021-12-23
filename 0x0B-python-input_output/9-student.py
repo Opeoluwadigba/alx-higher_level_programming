c7c9fae29746ab56e15012c0f2771ee8509a2a74
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-23 15:08:47 +0000

    student class.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-"""Module 9-student.py"""
+"""
+Module 11-student
+Contains class Student
+that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that retrieves its dictionary representation
+"""
 
 
 class Student():
-    """Class that defines a student"""
-    def__init__(self, first_name, last_name, age):
-       self.first_name = first_name
-       self.last_name = last_name
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """retrieves a dictionary representation of a Student instance."""
-        return vars(self)
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+        """
+        return self.__dict__
