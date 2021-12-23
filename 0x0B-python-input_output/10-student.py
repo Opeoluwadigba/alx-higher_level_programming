842655c48f5550447b3dbaa4f828de1d398c341e
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-23 15:15:45 +0000

    12th student class.

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,44 @@
-#!/usr/bin/python 3
-"""Module 10-student.py"""
+#!/usr/bin/python3
+"""
+Module 12-student
+Contains class Student
+that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that returns dictionary representation
+of requested attributes or all if none were requested
+"""
 
 
 class Student():
-    """Class that defines a student"""
-    def_init_(self, first_name, last_name, age):
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self, attrs=None):
-    dic_json = vars(self)
-    if isinstance(attrs, list):
-        nd = {}
-        for key in dic_json:
-            for i in attrs:
-                if key is i: 
-                    nd[key] = dic_json[key]
-        return nd
-    else:
-        return dic_json
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+        Return:
+            Only return dict of attrs given to us
+            Return entire dict if no attrs given
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
