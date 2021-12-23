09a7d834065fd8b97a13bbc7ce01e9427bb60082
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-23 15:01:10 +0000

    JSON class.

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Module 8-class_to_json.py"""
-
+"""
+Module 10-class_to_json
+Contains function that
+returns dictionary description with simple data structure
+(list, dictionary, dictionary, string)
+for JSON serialization of an object
+"""
 
 
 def class_to_json(obj):
-"""
-This Function returns to the dictionary descrption
-with simple a data structure
-"""
-
-return vars(obj)
- 
+    """Returns dictionary description with simple data structure
+       (list, dictionary, dictionary, string)
+       for JSON serialization of an object
+    Args:
+        obj: python object
+    """
+    return obj.__dict__
