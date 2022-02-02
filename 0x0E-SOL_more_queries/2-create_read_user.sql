cf8ccaf8f22ac692dae95b4a90af23a3f93582bb
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2022-02-02 02:38:21 -0800

    User 2 and privileges

--- 0x0E-SOL_more_queries/2-create_read_user.sql
+++ 0x0E-SOL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create user 'user_0d_2' and database 'hbtn_0d_2'
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT PRIVILEGE ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@'localhost';
