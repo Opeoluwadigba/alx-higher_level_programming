9dce5c70baea6c699ca5a0ca47dc88b74c30ae4d
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2022-02-02 16:02:09 -0800

    zero links

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- Show all second_table records
-ELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
+USE DATABASE hbtn_0c_0;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
